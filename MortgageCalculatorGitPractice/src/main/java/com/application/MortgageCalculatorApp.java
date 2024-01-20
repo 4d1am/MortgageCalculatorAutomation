2e85dc82c7e9fd7549eb89076d6da3530a6ebb49
4d1am <sunsworkworks@gmail.com>
2024-01-20 11:36:22 -0500

    Add print method

--- MortgageCalculatorGitPractice/src/main/java/com/application/MortgageCalculatorApp.java
+++ MortgageCalculatorGitPractice/src/main/java/com/application/MortgageCalculatorApp.java
@@ -6,5 +6,15 @@ package com.application;
 public class MortgageCalculatorApp {
 	public static void main(String[] args) {
 		System.out.println("Mortgage calculator application is starting");
+		
+		printDots(30);
+	
+	}
+	
+	private static void printDots(int n) {
+		for (int i = 0; i < n; i++) {
+			System.out.print("*");
+		}
+		System.out.println();
 	}
 }
