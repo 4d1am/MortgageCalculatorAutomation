d6438dacf3812e2e972669e50a71f1a3b941e135
4d1am <sunsworkworks@gmail.com>
2024-01-20 11:30:05 -0500

    Add a main method and print

--- MortgageCalculatorGitPractice/src/main/java/com/application/MortgageCalculatorApp.java
+++ MortgageCalculatorGitPractice/src/main/java/com/application/MortgageCalculatorApp.java
@@ -1,5 +1,10 @@
 package com.application;
 
+/**
+ * Create a class MortgageCalculator App
+ */
 public class MortgageCalculatorApp {
-
+	public static void main(String[] args) {
+		System.out.println("Mortgage calculator application is starting");
+	}
 }
